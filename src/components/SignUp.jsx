6082d78ac138efb826324faba91960e4e842b337
Aflah Paruthicode <aflutech2@gmail.com>
2025-08-16 20:28:05 +0530

    Signup reasons to join section

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,9 +2,10 @@ import Header from './Header';
 import Slider  from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { trendMovies } from '../utils/trend10Movies';
+import { reasonsToJoin, trendMovies } from '../utils/constants';
 
 const SignUp = () => {
+
     const settings = {
     dots: false,
     infinite: false,
@@ -12,11 +13,12 @@ const SignUp = () => {
     slidesToShow: 4.5,
     slidesToScroll: 4
   };
+
   return (
     <div className='w-full'>
             <Header />
             <div className=' brightness-[35%] h-[90vh] w-full'>
-            <img className='w-full h-[99vh] object-cover object-[2%_19%]' src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg" alt="" />
+                <img className='w-full h-[99vh] object-cover object-[2%_19%]' src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg" alt="" />
             </div>
 
             <div className='absolute top-[338px] left-0 right-0 mx-auto h-full text-white text-center bg-gradient-to-t from-black'>
@@ -24,50 +26,31 @@ const SignUp = () => {
                 <p className='text-[22px] font-semibold mb-7'>Starts at ₹149. Cancel at any time.</p>
                 <p className='font-normal mb-3 text-[16.5px]'>Ready to watch? Enter your email to create or restart your membership.</p>
                 <form action="" className='flex justify-center items-center gap-2'>
-            <input className='bg-[#1a1a1ab6] py-2 px-8 rounded-sm border w-[450px] h-[55px] text-[#888888] font-semibold' placeholder='Email or mobile number' type="text" />
-            <input className='bg-red-600 py-2 px-8 font-semibold text-[25px] text-white rounded-sm' value='Get Started  >' type="submit" />
+                    <input className='bg-[#1a1a1ab6] py-2 px-8 rounded-sm border w-[450px] h-[55px] text-[#888888] font-semibold' placeholder='Email or mobile number' type="text" />
+                    <input className='bg-red-600 py-2 px-8 font-semibold text-[25px] text-white rounded-sm' value='Get Started  >' type="submit" />
                 </form>
-
             </div>
-{/* bg-gradient-to-r from-[#210d16] from-15% via-[#b82869] via-[#e50914] via-[#b82869] to-[#210d16] to-84% = that gradient */}
-{/* 6.25rem = height */}
                         
-                <div class="relative bg-transparent h-[6.25rem] top-9 z-10 overflow-hidden
-                before:content-[''] 
-                before:left-[-10%]
-                before:w-[120%]
-                before:h-[12.80rem]
-                before:absolute
-                before:rounded-t-[100%] 
-                before:[background:linear-gradient(to_right,rgba(33,13,22,1)_16%,rgba(184,40,105,1),rgba(229,9,20,1),rgba(184,40,105,1),rgba(33,13,22,1)_84%)]
-                before:-z-10
-                
-                after:[background:radial-gradient(59%_495%_at_50%_-455%,rgba(64,97,231,0.4)_88%,rgba(0,0,0,0.1)_100%),_black]
-                after:absolute
-                after:w-[120%]
-                after:left-[-10%]
-                after:overflow-hidden
-                after:top-[4.5px]
-                after:rounded-t-[50%]
-                after:mx-auto
-                after:h-[12.60rem]
-                after:object-cover
-                after:z-10"
-                >
-                    <div className='absolute top-[58px] left-[350px] z-20'>
-
+            <div class="relative bg-transparent h-[6.25rem] top-9 z-10 overflow-hidden
+            before:content-[''] before:left-[-10%] before:w-[120%] before:-z-10
+            before:h-[12.80rem] before:absolute before:rounded-t-[100%] 
+            before:[background:linear-gradient(to_right,rgba(33,13,22,1)_16%,rgba(184,40,105,1),rgba(229,9,20,1),rgba(184,40,105,1),rgba(33,13,22,1)_84%)]
+            
+            after:[background:radial-gradient(59%_495%_at_50%_-455%,rgba(64,97,231,0.4)_88%,rgba(0,0,0,0.1)_100%),_black]
+            after:absolute after:w-[120%] after:left-[-10%] after:overflow-hidden
+            after:top-[4.5px] after:rounded-t-[50%] after:mx-auto after:h-[12.60rem] after:object-cover after:z-10" >
+                <div className='absolute top-[58px] left-[350px] z-20'>
                     <h1 className='text-white text-[32px] font-bold'>Trending Now</h1>
-                    </div>
+                </div>
             </div>
 
-
-            <div className="w-full flex justify-center h-[500px] bg-black">
+            <div className="w-full flex justify-center h-[430px] bg-black mt-2">
                 <div className='absolute w-[1200px] pt-8 pl-2'>
                     <Slider {...settings} >
                         {
                             trendMovies.map((movie) => {
                                 return (
-                                    <div className='w-10 py-8 px-3 relative '>
+                                    <div className='w-10 py-8 px-3 relative hover:scale-105 transition delay-100 duration-300 ease-in-out'>
                                         <img className='w-[90%] rounded-lg' src={movie.img} alt="" />
                                         <h1 className='absolute font-bold text-9xl text-black top-50 left-[-10px] [-webkit-text-stroke:2px_white] font-[poppins] text-shadow-[10px_7px_6px_var(--tw-text-shadow-color,_rgb(0_0_0_/_0.5))]'>{movie.position}</h1>
                                     </div>
@@ -75,6 +58,32 @@ const SignUp = () => {
                             })
                         }
                     </Slider>
+                </div>
+            </div>
+            <div className='flex justify-center bg-black h-[430px]'>
+                <div className='w-[1200px]  text-white bg-black'>
+                    <h1 className='text-white text-[32px] font-bold mb-3'>More reasons to join</h1>
+                    <div className='flex gap-4'>
+                       { 
+                            reasonsToJoin.map((reason) => {
+                                return (
+                                    <div className=' relative w-1/4 h-80 rounded-2xl py-9 px-6 bg-gradient-to-br from-[#653fad6c] to-[#1e001636]'>
+                                        <h1 className='font-bold text-2xl mb-5'>{reason.header}</h1>
+                                        <p className=' font-medium leading-5 text-[#ffffffb3]'>{reason.desc}</p>
+                                        <img className='absolute bottom-5 right-4' src={reason.img} alt="" />
+                                    </div>
+                                )
+                            })
+                       }
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-center bg-black h-[430px]'>
+                <div className='w-[1200px]  text-white bg-black'>
+                    <h1 className='text-white text-[32px] font-bold mb-3 mt-2'>Frequently Asked Questions</h1>
+                    <div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,95 @@
+export const trendMovies = [
+    {
+        position : 1,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABRhpHnfh6ia4QcotnBIjOTFL15LZcxTPjXf5VFU_E_ayrXSswVPM_vAfqrmSoUqf-gC_bRjsC6918GtEasl95dPq6PmppVAHtndE5xrFBIzsX0JVN6c9w-0AVT1lkdh3b6Xa.webp?r=2b8'
+    },
+    {
+        position : 2,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABVKTZpbyHQI0N2kXMP31Z0v0yq3J9PqPOovjwGVekpclbT9d0TtL54L8Yyo0nqKnVT9h3PSYtmKkU3Vi25WL806VRQOjD7VToW0EHjOZcv0DMJ58JO4PT5H9y198ct7wz01M.webp?r=79f'
+    },
+    {
+        position : 3,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABU73BvQjJgydQFEJ-vZZw9QtO8fL6lIxc71x9tOqxgDWh_BHokCWPIbGC5AxJjDk0D_sIH5KhmmYm9HLr3DKUXg8eo6nzF0rtSI.webp?r=d95'
+    },
+    {
+        position : 4,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABaW57jWqw0Gs9gqgmW816THwyNfPqphIiruQRFy9KXtt9PTPJpB4kzDKriPcpNjPKCj0itu1Yfjl-CR8rGEokBHPKE4O9lZQHXHRRpUHEelwoKk8lOf4gqlws-HnR2iZeSk4.webp?r=609'
+    },
+    {
+        position : 5,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABace3lfDYdvDP5GhS8n6Djz3jW0sjZtFLOYIGR9fQQS48CE1DC8PQ8-DlTy5qKPDPjeQcfX7AVJzFKz2zW2lsbUq5yiYN0g1NmTRk6F4dajOtAdVKLn_bBW6tGbQr22e74Mx.webp?r=fb9'
+    },
+    {
+        position : 6,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABfQbkxRwQV_zSeEDI92Hhu86ttq6vTI-NPSom5LdKyBMDQnH1igsOb6RDax9ISrNh5kO1_moHkhPGwegaKDB6vjbSWkpcddy-AGeaMkLfCDuBReX6VSfydIvc-9dldHeD-dw.webp?r=453'
+    },
+    {
+        position : 7,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABYUCgDPIk4LvnEw_j-dC5ca65oxm4yP2Z7RFKlRcN6WYzCwNA4MhTNqTRrSAK9zND3cSjrYEpbQculmTCXPD09ZbvdCosdbuOjc.webp?r=062'
+    },
+    {
+        position : 8,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABVVzJ7Q5ewWWr7Ju9Vpu5gUq1Guy_hTrvChxJceb-sKqWPiIs1YGHsy_iC2mNT5bKs2BepshCHriwd7IQhYM2HPuqd7MBfqMXFL8nXJshtQEi6-KAKZAcSvG7xE3daK4ABuC.webp?r=81a'
+    },
+    {
+        position : 9,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABQmIFk_PeW2I2K3n3xcYkRA_gWBZgvkWWdxPSrRcHKqUUivmZMwEsmlO6LEeiEjGUOOdqwpEa3s6ICacFfqZ8SnqF0q4lU4cl1c.webp?r=748'
+    },
+    {
+        position : 10,
+        img : 'https://occ-0-4822-3663.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABYGL4_ns5MGTE5-Yd3ZQVB5apyY9yxQ39lxjT_27ehdoGzrxRwNc4osgXEeifcv9yO_MgMySL-p6hMxtQZnIEkZmyglIJ7wm7ik.webp?r=c23'
+    }
+];
+
+export const reasonsToJoin = [
+    {
+        header : 'Enjoy on your TV',
+        desc : 'Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.',
+        img : '/enjoyOnTV.svg'
+    },
+    {
+        header : 'Download your shows to watch offline',
+        desc : 'Save your favourites easily and always have something to watch.',
+        img : '/download.svg'
+    },
+    {
+        header : 'Watch everywhere',
+        desc : 'Stream unlimited movies and TV shows on your phone, tablet, laptop and TV.',
+        img : '/watchEverywhere.svg'
+    },
+    {
+        header : 'Create profiles for kids',
+        desc : 'Send kids on adventures with their favourite characters in a space made just for them — free with your membership.',
+        img : '/createProfile.svg'
+    }
+];
+
+export const FAQ = [
+    {
+        question : 'What is Netflix?',
+        answer : `Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.
+                  You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!`
+    },
+    {
+        question : 'How much does Netflix cost?',
+        answer : `Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.`
+    },
+    {
+        question : 'Where can i watch?',
+        answer : `Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+                  You can also download your favourite shows with the iOS or Android app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.`
+    },
+    {
+        question : 'How do i cancel?',
+        answer : `Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`
+    },
+    {
+        question : 'What can i watch on Netflix?',
+        answer : `Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.`
+    },
+    {
+        question : 'Is Netflix good for kids?',
+        answer : `The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
+                  Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.`
+    }
+];
